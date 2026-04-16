5b6d1033b3d2729a94fa7a8159a9e4062bdad3bd
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-04-17 01:35:13 +0530

    add: init

---
+++ src/test/java/com/avijeet/rebase/RebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avijeet.rebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
